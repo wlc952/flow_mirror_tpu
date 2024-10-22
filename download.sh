b9e413527686e1df70fd60c61c34ceccdbaaee88
Leander_W <wlc@zju.edu.cn>
2024-10-22 15:14:37 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -4,7 +4,7 @@ set -e
 
 MODEL_DIR="models"
 MODEL_ARCHIVE="models.tar.gz"
-MODEL_URL="https://github.com/wlc952/flow_mirror_tpu/releases/download/v0.1/models.tar.gz"
+MODEL_URL="https://modelscope.cn/models/wlc952/flow_mirror_aigchub/resolve/master/models.tar.gz"
 
 if [ ! -d "$MODEL_DIR" ]; then
     if [ ! -f "$MODEL_ARCHIVE" ]; then
